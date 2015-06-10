7974b32c61890ce66756fc42969b6f4f75c474ee
Curtiss Melvin <CurtissMelvin@users.noreply.github.com>
2015-06-10 11:21:14 -0700

    sidebar-blog.php

---
+++ sidebar-blog.php
@@ -0,0 +1,21 @@
+<aside id="blog-sidebar" class="asideleft">
+
+	<?php if (is_active_sidebar ('blog')): ?>
+    
+		<?php dynamic_sidebar('blog'); ?>
+        
+	<?php else : ?>
+ <h2></h2>
+<br />
+<p>We strive to ensure all of our customers' goals and needs are fully realized, and that open, upfront and honest communication takes place throughout the entire remodeling and construction process.</p>
+
+<p>We attain these goals by following these principles:</p>
+<ul>
+    <li>Be sure that every relationship we enter into is a Win-Win</li>
+    <li>Treat individuals with respect</li>
+    <li>Be sure that the solution we propose is the most effective at meeting our customer's needs</li>
+    <li>Stay abreast of the regarded best practices and materials in construction standards and continually searching for ways that we can further "green" our process</li><br />
+</ul>
+<p>If you'd like to learn more about us and find out how we can help your family on your next home renovation project, we would love to hear from you.</p>
+<?php endif; ?>
+</aside>
