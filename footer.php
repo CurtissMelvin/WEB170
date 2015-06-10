8285bc6480b67030d10e44a1f7c953368e8f4a23
Curtiss Melvin <CurtissMelvin@users.noreply.github.com>
2015-06-10 11:19:54 -0700

    footer.php

---
+++ footer.php
@@ -0,0 +1,20 @@
+ 
+  
+</div> <!---MIDDLE  DIV END-->
+
+  
+       
+    <!--FOOTER-->
+<footer>
+<?php print ("&copy; " . date ('Y') . " "); ?>Website by Curtiss Melvin <a href="http://www.curtissmelvin.com/portfolio/" target="_blank">Northwest Design & Construction </a>&bull; <?php wp_loginout(); ?> 
+</footer>
+
+	<!--FOOTER END-->
+    
+</div> <!--WRAPPER DIV END-->
+<?php /* wp_footer function must be last item before closing body tag: many plugins use this hook to reference JavaScript files */ ?>
+<?php wp_footer(); ?>  
+</body>
+</html>
+
+
